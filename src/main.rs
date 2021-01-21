cc40ff47e98406e9adc161a56518c2fda729874c
Jules Youngberg <julesdayoungberg@gmail.com>
2021-01-20 20:04:25 -0800

    basic audio listener

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,58 @@
+use cpal;
+use cpal::traits::DeviceTrait;
 use nannou::prelude::*;
+use std::sync::mpsc::channel;
+use std::time;
+
+mod util;
 
 fn main() {
-    nannou::app(model).update(update).simple_window(view).run();
+    nannou::app(model).run();
 }
 
-struct Model {}
-
-fn model(_app: &App) -> Model {
-    Model {}
+#[allow(dead_code)]
+struct Model {
+    rx: std::sync::mpsc::Receiver<Vec<f32>>,
+    stream: cpal::Stream,
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
+fn model(app: &App) -> Model {
+    // Create a window to receive key pressed events.
+    app.new_window().view(view).build().unwrap();
 
-fn view(_app: &App, _model: &Model, frame: Frame) {
+    // Initialise the audio host so we can spawn an audio stream.
+    println!("configuring audio input device");
+    let audio_device = util::get_audio_device();
+    let mic_config = util::get_mic_config(&audio_device);
+    let cpal::SampleRate(sample_rate) = mic_config.sample_rate();
+    println!("sample_rate: {:?}", sample_rate);
+
+    let (tx, rx) = channel();
+
+    // create the stream
+    let stream = audio_device
+        .build_input_stream(
+            &mic_config.config(),
+            move |data: &[f32], _: &cpal::InputCallbackInfo| match tx.send(data.to_vec()) {
+                Ok(()) => (),
+                Err(e) => panic!(e),
+            },
+            move |err| {
+                panic!(err);
+            },
+        )
+        .unwrap();
+
+    Model { rx, stream }
+}
+
+fn view(_app: &App, model: &Model, frame: Frame) {
     frame.clear(PURPLE);
+
+    let data = match model.rx.recv_timeout(time::Duration::from_millis(1)) {
+        Ok(m) => m,
+        Err(_) => return,
+    };
+
+    println!("received: {:?}", data.len());
 }
---
+++ src/util.rs
@@ -0,0 +1,19 @@
+use cpal::traits::{DeviceTrait, HostTrait};
+use cpal::{Device, SupportedStreamConfig};
+
+pub fn get_audio_device() -> Device {
+    let host = cpal::default_host();
+    host.default_input_device()
+        .expect("no audio input device available")
+}
+
+pub fn get_mic_config(device: &Device) -> SupportedStreamConfig {
+    // get supported config
+    let mut supported_configs_range = device
+        .supported_input_configs()
+        .expect("error while querying audio configs");
+    supported_configs_range
+        .next()
+        .expect("no supported audio config?!")
+        .with_max_sample_rate()
+}
